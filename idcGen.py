6f5dcd5760d241aba1a8fbf62a255642a7a61882
Lionnus Kesting <lionnuzzz@gmail.com>
2024-04-07 22:15:22 +0200

    Fixed SMD pad DRC error, added fingerWidth param

--- idcGen.py
+++ idcGen.py
@@ -6,9 +6,25 @@ Interdigitated Capacitor Layout Generator for KiCad
 This script generates the layout for an interdigitated capacitor based on user-defined parameters.
 The generated layout is saved in a KiCad footprint file in the ./idc.pretty directory.
 
+Parameters:
+- modulename: The name of the module.
+- trackWidth: Width of each track/finger in mm.
+- gap: Gap between tracks/fingers in mm.
+- totalWidth: Total width of the capacitor in mm.
+- fingerLength: Length of each finger in mm. Overwrites the totalWidth parameter.
+- numFingers: Number of fingers/tracks.
+- layer: The PCB layer the capacitor is on.
+- connectingTrackWidth: Width of the connecting horizontal tracks in mm (default: trackWidth).
+
 Usage:
     python3 idcGen.py -h
+    # For fixed total width:
     python3 idcGen.py --modulename <name> --trackWidth <width> --gap <gap> --totalWidth <totalWidth> --numFingers <numFingers>
+    # For fixed finger length:
+    python3 idcGen.py --modulename <name> --trackWidth <width> --gap <gap> --fingerLength <fingerLength> --numFingers <numFingers>
+    
+Example:
+    python3 idcGen.py --modulename IDC --trackWidth 0.8 --gap 0.5 --fingerLength 15 --numFingers 10 --connectingTrackWidth 0.8
 
 Output file path:
     ./idc.pretty/
@@ -21,7 +37,7 @@ import argparse
 from datetime import datetime
 
 
-def idcGen(modulename, trackWidth, gap, totalWidth, numFingers, layer='F.Cu', connectingTrackWidth=None):
+def idcGen(trackWidth, gap, totalWidth, numFingers, fingerLength=None, modulename='IDC', layer='F.Cu', connectingTrackWidth=None):
     """
     Generates the layout for an interdigitated capacitor.
 
@@ -30,6 +46,7 @@ def idcGen(modulename, trackWidth, gap, totalWidth, numFingers, layer='F.Cu', co
     - trackWidth: Width of each track/finger in mm.
     - gap: Gap between tracks/fingers in mm.
     - totalWidth: Total width of the capacitor in mm.
+    - fingerLength: Length of each finger in mm. Overwrite the totalWidth parameter.
     - numFingers: Number of fingers/tracks.
     - layer: The PCB layer the capacitor is on.
     - connectingTrackWidth: Width of the connecting horizontal tracks in mm (default: trackWidth).
@@ -45,28 +62,30 @@ def idcGen(modulename, trackWidth, gap, totalWidth, numFingers, layer='F.Cu', co
 
     # Calculate the total length and the finger length
     totalHeight = numFingers * (trackWidth + gap) - gap
-    fingerLength = totalWidth - connectingTrackWidth * 2 - gap
-
-    # Start building the module layout
-    mod = ""
-    mod += "(module %s (layer %s) (tedit %X)\n" % (modulename, layer, seconds)
-
-    # Add connecting tracks
-    mod += "  (fp_rect (start %f %f) (end %f %f) (layer F.Cu))\n" % (
-        0, 0, connectingTrackWidth, totalHeight)
-    mod += "  (pad 1 smd rect (at %f %f) (size %f %f) (layers F.Cu))\n" % (
-        connectingTrackWidth/2, -connectingTrackWidth/2, connectingTrackWidth, connectingTrackWidth)
-    mod += "  (fp_rect (start %f %f) (end %f %f) (layer F.Cu))\n" % (
-        totalWidth - connectingTrackWidth, 0, totalWidth, totalHeight)
-    mod += "  (pad 2 smd rect (at %f %f) (size %f %f) (layers F.Cu))\n" % (
-        totalWidth-connectingTrackWidth/2, -connectingTrackWidth/2, connectingTrackWidth, connectingTrackWidth)
+    if fingerLength is None:
+        fingerLength = totalWidth - connectingTrackWidth * 2 - gap
+    else: 
+        totalWidth = fingerLength + 2 * connectingTrackWidth + gap
     
-    # Add fingers
+    # Add info to the header
+    mod = ""
+    # Add module header
+    mod += "(footprint %s (layer %s) (tedit %X)\n" % (modulename, layer, seconds)
+
+    # Add connecting tracks as SMD pads
+    mod += "  (pad 1 smd rect (at %f %f) (size %f %f) (layers F.Cu))\n" % (
+        connectingTrackWidth/2, totalHeight/2, connectingTrackWidth, totalHeight)
+    mod += "  (pad 2 smd rect (at %f %f) (size %f %f) (layers F.Cu))\n" % (
+        totalWidth-connectingTrackWidth/2, totalHeight/2, connectingTrackWidth, totalHeight)
+    
+    # Add fingers as SMD pads
     for n in range(numFingers):
         posX = connectingTrackWidth + (n % 2) * gap
         posY = n * (trackWidth + gap)
-        mod += "  (fp_rect (start %f %f) (end %f %f) (layer F.Cu))\n" % (
-            posX, posY, posX + fingerLength, posY + trackWidth)
+        sizeX = fingerLength
+        sizeY = trackWidth
+        mod += "  (pad %X smd rect (at %f %f) (size %f %f) (layers F.Cu))\n" % (
+            n%2 + 1, posX + sizeX / 2, posY + sizeY / 2, sizeX, sizeY)
     mod += ")\n"
 
     # Print terminal message with dimensions
@@ -82,7 +101,8 @@ def main():
     parser.add_argument("-m", "--modulename", required=True, help="Module name for the generated capacitor.")
     parser.add_argument("-t", "--trackWidth", required=True, type=float, help="Width of each track/finger in mm.")
     parser.add_argument("-g", "--gap", required=True, type=float, help="Gap between tracks/fingers in mm.")
-    parser.add_argument("-w", "--totalWidth", required=True, type=float, help="Total width of the capacitor in mm.")
+    parser.add_argument("-w", "--totalWidth", type=float, help="Total width of the capacitor in mm.")
+    parser.add_argument("-f", "--fingerLength", type=float, help="Length of each finger in mm. Overwrites the totalWidth parameter.")
     parser.add_argument("-n", "--numFingers", required=True, type=int, help="Number of fingers/tracks.")
     parser.add_argument("-c", "--connectingTrackWidth", type=float, help="Width of the connecting tracks in mm (default: same as trackWidth).")
 
@@ -97,16 +117,24 @@ def main():
         print("Error: Gap must be greater than 0.")
         return
 
-    if args.totalWidth <= 0:
-        print("Error: Total width must be greater than 0.")
-        return
-
     if args.numFingers < 2:
         print("Error: Number of fingers must be at least 2.")
         return
 
     if args.connectingTrackWidth is not None and args.connectingTrackWidth <= 0:
         print("Error: Connecting track width must be greater than 0.")
+        return
+    
+    if args.totalWidth is None and args.fingerLength is None:
+        print("Error: Either total width or finger length must be provided.")
+        return
+    
+    if args.totalWidth is not None and args.totalWidth <= 0:
+        print("Error: Total width must be greater than 0.")
+        return
+    
+    if args.fingerLength is not None and args.fingerLength <= 0:
+        print("Error: Finger length must be greater than 0.")
         return
 
     # Generate the capacitor layout
@@ -115,6 +143,7 @@ def main():
         trackWidth=args.trackWidth,
         gap=args.gap,
         totalWidth=args.totalWidth,
+        fingerLength=args.fingerLength,
         numFingers=args.numFingers,
         connectingTrackWidth=args.connectingTrackWidth
     )
@@ -125,10 +154,18 @@ def main():
         os.makedirs(output_dir)
 
     # Construct the output filename with parameters for traceability
-    output_filename = f"{output_dir}/{args.modulename}_tw{args.trackWidth}_ctw{args.connectingTrackWidth}_g{args.gap}_w{args.totalWidth}_n{args.numFingers}.kicad_mod"
+    if args.fingerLength is not None:
+        output_filename = f"{output_dir}/{args.modulename}_tw{args.trackWidth}_ctw{args.connectingTrackWidth}_g{args.gap}_fl{args.fingerLength}_n{args.numFingers}.kicad_mod"
+    else:
+        output_filename = f"{output_dir}/{args.modulename}_tw{args.trackWidth}_ctw{args.connectingTrackWidth}_g{args.gap}_w{args.totalWidth}_n{args.numFingers}.kicad_mod"
+    
     with open(output_filename, 'w') as f:
         f.write("# ----------------------------------------------------\n")
-        f.write("# Autogenerated by idcGen.py\n")
+        f.write("# Autogenerated by idcGen.py, written by Lionnus Kesting.\n")
+        f.write("# Code available at: https://github.com/lionnus/idc_kicad.\n")
+        f.write("# ----------------------------------------------------\n")
+        f.write("# Date: %s\n" % datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+        f.write("# Parameters: %s\n" % args)
         f.write(f"# {dimensions}\n")
         f.write("# ----------------------------------------------------\n")
         f.write(cap)
